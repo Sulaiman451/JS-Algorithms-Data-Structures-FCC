f0a7625feae3cf6311c062a1c70620887a774896
Sulaiman Rahman <rsulaiman451@gmail.com>
2023-05-15 20:22:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## The Task
+
+A function called countdown has been defined with one parameter (n). The function should use recursion to return an array containing the integers n through 1 based on the n parameter.
+
+If the function is called with a number less than 1, it should return an empty array. The function must use recursion by calling itself and must not use loops of any kind. Refer to recursionCountdown.js to see my solution, comments, and at the end of the file the original problem.
---
+++ recursionCountdown.js
@@ -0,0 +1,22 @@
+function countdown(n) {
+  // Return an empty array if n < 1
+  if (n < 1) {
+    return [];
+    /* Otherwise, create an array and recurse within
+    that array, call the function with (n - 1) until
+    n < 1, and then the function will "stack" all those within the array that is returned*/
+  } else {
+    let arr = countdown(n - 1);
+    // Push each value within the array to the front
+    arr.unshift(n);
+    return arr;
+  }
+}
+
+/*
+// Only change code below this line
+function countdown(n){
+  return;
+}
+// Only change code above this line
+*/
