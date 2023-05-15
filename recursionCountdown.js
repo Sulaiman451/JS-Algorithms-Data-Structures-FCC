f4f788a93e6742f30ebd914e85704114d8e75e4d
Sulaiman <rsulaiman451@gmail.com>
2023-05-15 20:25:14 +0100

    Update recursionCountdown.js

--- recursionCountdown.js
+++ recursionCountdown.js
@@ -3,7 +3,7 @@ function countdown(n) {
   if (n < 1) {
     return [];
     /* Otherwise, create an array and recurse within that array, call the function with (n - 1) until
-    n < 1, and then the function will "stack" all those within the array that is returned*/
+    n < 1, and then the function will "stack" all those within the array that is returned */
   } else {
     let arr = countdown(n - 1);
     // Push each value within the array to the front
